47311e33eab462076fe7d3e1cb4609c4b5a406a7
René Schwaiger <sanssecours@me.com>
2025-03-11 14:41:46 +0100

    EEPROM: Add coroutine to write x-axis offset

--- icotronic/can/eeprom/sth.py
+++ icotronic/can/eeprom/sth.py
@@ -113,6 +113,43 @@ class STHEEPROM(SensorNodeEEPROM):
 
         return await self.read_float(address=8, offset=4)
 
+    async def write_x_axis_acceleration_offset(self, offset: int) -> None:
+        """Write the acceleration offset of the x-axis to the EEPROM
+
+        Parameters
+        ----------
+
+        offset:
+            The (negative) offset of the acceleration value in multiples of g₀
+
+        Example
+        -------
+
+        >>> from asyncio import run
+        >>> from math import isclose
+        >>> from icotronic.can.connection import Connection
+        >>> from icotronic.can.sth import STH
+
+        Write and read the acceleration offset of STH 1
+
+        >>> async def write_read_x_axis_acceleration_offset(offset):
+        ...     async with Connection() as stu:
+        ...         # We assume that at least one sensor device is available
+        ...         async with stu.connect_sensor_device(0, STH) as sth:
+        ...             await sth.eeprom.write_x_axis_acceleration_offset(
+        ...                 offset)
+        ...             return (await
+        ...                 sth.eeprom.read_x_axis_acceleration_offset())
+        >>> acceleration_difference_max = 200
+        >>> offset = -(acceleration_difference_max/2)
+        >>> offset_read = run(write_read_x_axis_acceleration_offset(offset))
+        >>> isclose(offset, offset_read)
+        True
+
+        """
+
+        await self.write_float(address=8, offset=4, value=offset)
+
 
 # -- Main ---------------------------------------------------------------------
 
@@ -120,7 +157,7 @@ if __name__ == "__main__":
     from doctest import run_docstring_examples
 
     run_docstring_examples(
-        STHEEPROM.read_x_axis_acceleration_offset,
+        STHEEPROM.write_x_axis_acceleration_offset,
         globals(),
         verbose=True,
     )
