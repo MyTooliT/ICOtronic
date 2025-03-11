e72c60a85742f463ff98a78ae26c4ec524bdc20c
René Schwaiger <sanssecours@me.com>
2025-03-11 16:07:33 +0100

    EEPROM: Add coroutine to read z-axis slope

--- icotronic/can/eeprom/sth.py
+++ icotronic/can/eeprom/sth.py
@@ -318,6 +318,44 @@ class STHEEPROM(SensorNodeEEPROM):
 
         return await self.read_float(address=8, offset=16)
 
+    async def write_z_axis_acceleration_slope(self, slope: float) -> None:
+        """Write the acceleration slope of the z-axis to the EEPROM
+
+        Parameters
+        ----------
+
+        slope:
+            The addition to the acceleration value for one step of the ADC in
+            multiples of g₀
+
+        Example
+        -------
+
+        >>> from asyncio import run
+        >>> from math import isclose
+        >>> from icotronic.can.connection import Connection
+        >>> from icotronic.can.sth import STH
+
+        Write and read the acceleration slope of STH 1
+
+        >>> async def write_read_z_axis_acceleration_slope(slope):
+        ...     async with Connection() as stu:
+        ...         # We assume that at least one sensor device is available
+        ...         async with stu.connect_sensor_device(0, STH) as sth:
+        ...             await sth.eeprom.write_z_axis_acceleration_slope(slope)
+        ...             return (await
+        ...                     sth.eeprom.read_z_axis_acceleration_slope())
+        >>> adc_max = 0xffff
+        >>> acceleration_difference_max = 200
+        >>> slope = acceleration_difference_max / adc_max
+        >>> slope_read = run(write_read_z_axis_acceleration_slope(slope))
+        >>> isclose(slope, slope_read)
+        True
+
+        """
+
+        await self.write_float(address=8, offset=16, value=slope)
+
 
 # -- Main ---------------------------------------------------------------------
 
@@ -325,7 +363,7 @@ if __name__ == "__main__":
     from doctest import run_docstring_examples
 
     run_docstring_examples(
-        STHEEPROM.read_z_axis_acceleration_slope,
+        STHEEPROM.write_z_axis_acceleration_slope,
         globals(),
         verbose=True,
     )
